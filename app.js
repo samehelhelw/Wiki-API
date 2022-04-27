30b3d49f464affe29a4fa0e9bac5d77877490360
samehelhelw <samehelhelw2@gmail.com>
2022-04-27 09:44:37 +0200

    RESTful-API final functions

--- app.js
+++ app.js
@@ -63,6 +63,61 @@ Article.find( function(err , foundArticles){
 
   //////////////////////////Requests targeting A specific Articles///////////////////
 
+  app.route("/articles/:articleTitle")
+
+  .get(function(req ,res){
+    Article.findOne({title: req.params.articleTitle} , function(err ,foundArticle){
+      if(foundArticle){
+        res.send(foundArticle);
+      }else{
+        res.send("No Articles matches that title is found ");
+      }
+    })
+  })
+  .put(function (req ,res){
+
+    Article.replaceOne(
+      {title: req.params.articleTitle},
+      {title: req.body.title ,content: req.body.content},
+      {overwrite: true},
+      function(err){
+        if(!err){
+          res.send("Succefully updated a specific article ");
+        }
+      }
+
+    );
+  })
+  .patch(function(req, res){
+
+    Article.updateOne(
+    {title: req.params.articleTitle},
+    {$set : req.body},
+    function(err){
+      if(!err){
+        res.send("Succefully updated a specific article ");
+      }
+    }
+  );
+
+  })
+  .delete(function(req ,res){
+
+   Article.deleteOne(
+     {title: req.params.articleTitle},
+     function(err){
+       if(!err){
+         res.send("Succefully deleted the corresponding article ");
+       }else {
+         res.send(err);
+       }
+     }
+   );
+
+  });
+
+
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
